c9c474c86d6d0aacb03f728fb1cc26135e9220c6
andreasmz <174248270+andreasmz@users.noreply.github.com>
2025-12-27 23:57:01 +0100

    found potential bugs

--- pypdfserver/__init__.py
+++ pypdfserver/__init__.py
@@ -3,11 +3,3 @@ pyPDFserver - A locally hosted scan destination to apply OCR and duplex to scans
 """
 __author__ = "Andreas Brilka"
 __version__ = "0.1.0"
-
-from .core import *
-from .server import PDF_FTPServer
-from .cmd import start_pyPDFserver
-
-logger.info(f"Loading pyPDFserver version {__version__}")
-
-pdf_server: PDF_FTPServer|None = None
--- pypdfserver/__main__.py
+++ pypdfserver/__main__.py
@@ -1,4 +1,11 @@
-from . import start_pyPDFserver
+from . import  __version__
+from .core import *
+from .server import PDF_FTPServer
+from .cmd import start_pyPDFserver
+
+logger.info(f"Loading pyPDFserver version {__version__}")
+
+pdf_server: PDF_FTPServer|None = None
 
 if __name__ == "__main__":
     start_pyPDFserver()
